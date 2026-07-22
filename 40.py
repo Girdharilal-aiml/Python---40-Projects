e20a3c0170d8c774087a5caebb93df9480544103
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-07-22 14:25:18 +0500

    Update 40.py

--- 40.py
+++ 40.py
@@ -444,6 +444,7 @@ class ReceiptScanner:
                 self.image_canvas.delete('all')
                 self.image_canvas.create_image(
                     self.image_canvas.winfo_width() // 2,
+                    self.image_canvas.winfo_height() // 2,
                 )
 
 
